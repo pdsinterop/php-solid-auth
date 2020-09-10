88d87a77ddeefa7c98f1c92edcfd2417b687169a
Ben Peachey <potherca@gmail.com>
2020-09-10 13:00:16 +0200

    Add Entity classes to be used with League\OAuth2\Server.

---
+++ src/Entity/Scope.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Pdsinterop\Solid\Auth\Entity;
+
+use League\OAuth2\Server\Entities\ScopeEntityInterface;
+use League\OAuth2\Server\Entities\Traits\EntityTrait;
+use League\OAuth2\Server\Entities\Traits\ScopeTrait;
+
+class Scope implements ScopeEntityInterface
+{
+    use EntityTrait;
+    use ScopeTrait;
+}
