ac9160751514212b6791ecc2b5a83f0b15376dc1
Ben Peachey <potherca@gmail.com>
2020-09-12 15:42:30 +0200

    Add required Server config keys to example.

--- tests/example.php
+++ tests/example.php
@@ -40,7 +40,35 @@ $config = (new \Pdsinterop\Solid\Auth\Factory\ConfigFactory(
     $clientSecret,
     $encryptionKey,
     $privateKey,
-    [/* @FIXME: Server config keys go here */]
+    [
+        /* URL of the OP's OAuth 2.0 Authorization Endpoint [OpenID.Core]. */
+        \Pdsinterop\Solid\Auth\Enum\OpenId\OpenIdConnectMetadata::AUTHORIZATION_ENDPOINT => 'https://server/authorize',
+
+        /* URL using the https scheme with no query or fragment component that
+         * the OP asserts as its Issuer Identifier. If Issuer discovery is
+         * supported, this value MUST be identical to the issuer value returned
+         * by WebFinger. This also MUST be identical to the iss Claim value in
+         * ID Tokens issued from this Issuer.
+         */
+        \Pdsinterop\Solid\Auth\Enum\OpenId\OpenIdConnectMetadata::ISSUER => 'https://server/identifier',
+
+        /* URL of the OP's JSON Web Key Set [JWK] document. This contains the
+         * signing key(s) the RP uses to validate signatures from the OP. The
+         * JWK Set MAY also contain the Server's encryption key(s), which are
+         * used by RPs to encrypt requests to the Server.
+         *
+         * When both signing and encryption keys are made available, a use
+         * (Key Use) parameter value is REQUIRED for all keys in the referenced
+         * JWK Set to indicate each key's intended usage. Although some
+         * algorithms allow the same key to be used for both signatures and
+         *  encryption, doing so is NOT RECOMMENDED, as it is less secure.
+         *
+         * The JWK x5c parameter MAY be used to provide X.509 representations
+         * of keys provided. When used, the bare key values MUST still be
+         * present and MUST match those in the certificate.
+         */
+        \Pdsinterop\Solid\Auth\Enum\OpenId\OpenIdConnectMetadata::JWKS_URI => 'https://server/jwk'
+    ]
 ))->create();
 
 $authorizationServer = (new \Pdsinterop\Solid\Auth\Factory\AuthorizationServerFactory($config))->create();
