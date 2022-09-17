78daf4ce3db795a4a76d7ed224edc5016c0f3031
Ben Peachey <ben@muze.nl>
2022-09-17 12:50:53 +0200

    Fixes whitespace.

--- src/TokenGenerator.php
+++ src/TokenGenerator.php
@@ -14,8 +14,9 @@ use Lcobucci\JWT\Signer\Rsa\Sha256;
 
 class TokenGenerator
 {
-    use CryptTrait;
     ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    use CryptTrait;
 
     /** @var Config */
     public $config;
@@ -28,7 +29,7 @@ class TokenGenerator
         $this->config = $config;
         $this->setEncryptionKey($this->config->getKeys()->getEncryptionKey());
     }
-	
+
 	public function generateRegistrationAccessToken($clientId, $privateKey) {
 		$issuer = $this->config->getServer()->get(OidcMeta::ISSUER);
 
@@ -42,7 +43,7 @@ class TokenGenerator
 
 		return $token->toString();
 	}
-		
+
 	public function generateIdToken($accessToken, $clientId, $subject, $nonce, $privateKey, $dpopKey=null) {
 		$issuer = $this->config->getServer()->get(OidcMeta::ISSUER);
 
@@ -75,7 +76,7 @@ class TokenGenerator
                         ->getToken($jwtConfig->signer(), $jwtConfig->signingKey());
                 return $token->toString();
 	}
-	
+
 	public function respondToRegistration($registration, $privateKey) {
 		/*
 			Expects in $registration:
@@ -94,10 +95,10 @@ class TokenGenerator
 			'token_endpoint_auth_method' => 'client_secret_basic',
 			'registration_access_token' => $registration_access_token,
 		);
-		
+
 		return array_merge($registrationBase, $registration);
 	}
-	
+
 	public function addIdTokenToResponse($response, $clientId, $subject, $nonce, $privateKey, $dpopKey=null) {
 		if ($response->hasHeader("Location")) {
 			$value = $response->getHeaderLine("Location");
@@ -111,7 +112,7 @@ class TokenGenerator
 					$privateKey,
 					$dpopKey
 				);
-				$value = preg_replace("/#access_token=(.*?)&/", "#access_token=\$1&id_token=$idToken&", $value);				
+				$value = preg_replace("/#access_token=(.*?)&/", "#access_token=\$1&id_token=$idToken&", $value);
 				$response = $response->withHeader("Location", $value);
 			} else if (preg_match("/code=(.*?)&/", $value, $matches)) {
 				$idToken = $this->generateIdToken(
@@ -153,12 +154,13 @@ class TokenGenerator
 	public function getCodeInfo($code) {
 		return json_decode($this->decrypt($code), true);
 	}
+
 	///////////////////////////// HELPER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 	private function generateJti() {
 		return substr(md5((string)time()), 12); // FIXME: generate unique jti values
 	}
-	
+
 	private function generateTokenHash($accessToken) {
 		$atHash = hash('sha256', $accessToken);
 		$atHash = substr($atHash, 0, 32);
