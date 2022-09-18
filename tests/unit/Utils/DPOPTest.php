36144c5a91a52127956c31aee48a2da18cfac6fc
Ben Peachey <ben@muze.nl>
2022-09-18 11:44:00 +0200

    Code cleanup
    - Change mixed tabs/spaces to spaces
    - Add trailing commas to arrays
    - Make method opening bracket consistent across methods
    - Remove trailing whitespaces
    - Add trailing newline to file

--- tests/unit/Utils/DPOPTest.php
+++ tests/unit/Utils/DPOPTest.php
@@ -13,42 +13,42 @@ use Lcobucci\JWT\Validation\RequiredConstraintsViolated;
 class DPOPTest extends TestCase
 {
 
-	private $dpop;
-	private $url;
-	private $serverRequest;
+    private $dpop;
+    private $url;
+    private $serverRequest;
 
-	protected function sign($dpop, $privateKey=null)
-	{
-		$keyPath    = __DIR__ . '/../../fixtures/keys';
-		if (!$privateKey) {
-	        $privateKey = file_get_contents($keyPath . '/private.key');
-	    }
+    protected function sign($dpop, $privateKey = null)
+    {
+        $keyPath = __DIR__ . '/../../fixtures/keys';
+        if (!$privateKey) {
+            $privateKey = file_get_contents($keyPath . '/private.key');
+        }
 
-		$signature = '';
+        $signature = '';
         $success = \openssl_sign(
             Base64Url::encode(json_encode($dpop['header'])).'.'.
-            Base64Url::encode(json_encode($dpop['payload'])), 
-            $signature, 
-            $privateKey, 
+            Base64Url::encode(json_encode($dpop['payload'])),
+            $signature,
+            $privateKey,
             OPENSSL_ALGO_SHA256
         );
 
         if (!$success) {
-        	throw new \Exception('unable to sign dpop');
+            throw new \Exception('unable to sign dpop');
         }
         $token = Base64Url::encode(json_encode($dpop['header'])).'.'.
-            	 Base64Url::encode(json_encode($dpop['payload'])).'.'.
-            	 Base64Url::encode($signature);
+            Base64Url::encode(json_encode($dpop['payload'])).'.'.
+            Base64Url::encode($signature);
 
         return array_merge($dpop, [
-        	'signature' => $signature,
-        	'token' => $token
+            'signature' => $signature,
+            'token' => $token,
         ]);
-	}
+    }
 
-	protected function setUp(): void
-	{
-		$keyPath    = __DIR__ . '/../../fixtures/keys';
+    protected function setUp(): void
+    {
+        $keyPath    = __DIR__ . '/../../fixtures/keys';
         $privateKey = file_get_contents($keyPath . '/private.key');
         $publicKey  = file_get_contents($keyPath . '/public.key');
 
@@ -56,13 +56,13 @@ class DPOPTest extends TestCase
         $jwk = [
             'kty' => 'RSA',
             'n' => Base64Url::encode($keyInfo['rsa']['n']),
-            'e' => Base64Url::encode($keyInfo['rsa']['e'])
+            'e' => Base64Url::encode($keyInfo['rsa']['e']),
         ];
 
         $header = [
             'typ' => 'dpop+jwt',
             'alg' => 'RS256',
-            'jwk' => $jwk
+            'jwk' => $jwk,
         ];
 
         $payload = [
@@ -72,22 +72,22 @@ class DPOPTest extends TestCase
             'htu' => 'https://www.example.com',
             'iat' => time(),
             'nbf' => time(),
-            'exp' => time()+3600
+            'exp' => time()+3600,
         ];
 
         $this->dpop = $this->sign([
-        	'header'  => $header,
-        	'payload' => $payload
+            'header' => $header,
+            'payload' => $payload,
         ]);
 
-		$this->url = 'https://www.example.com';
+        $this->url = 'https://www.example.com';
         $this->serverRequest = new \Laminas\Diactoros\ServerRequest(array(),array(), $this->url);
+    }
 
-	}
-
-	private function getWrongKey() {
-        $keyPath    = __DIR__ . '/../../fixtures/keys';
-        $wrongKey  = file_get_contents($keyPath . '/wrong.key');
+    private function getWrongKey()
+    {
+        $keyPath  = __DIR__ . '/../../fixtures/keys';
+        $wrongKey = file_get_contents($keyPath . '/wrong.key');
 
         $keyInfo = \openssl_pkey_get_details(\openssl_pkey_get_public($wrongKey));
         return $keyInfo;
@@ -96,7 +96,7 @@ class DPOPTest extends TestCase
     /**
      * @covers ::validateDpop
      */
-	public function testWrongTyp(): void
+    public function testWrongTyp(): void
     {
         $this->dpop['header']['typ'] = 'jwt';
         $token = $this->sign($this->dpop);
@@ -111,7 +111,7 @@ class DPOPTest extends TestCase
     /**
      * @covers ::validateDpop
      */
-    public function testAlgNone(): void 
+    public function testAlgNone(): void
     {
         $this->dpop['header']['alg'] = 'none';
         $token = $this->sign($this->dpop);
@@ -132,17 +132,17 @@ class DPOPTest extends TestCase
         $this->dpop['header']['jwk'] = [
             'kty' => 'RSA',
             'n' => Base64Url::encode($theWrongKey['rsa']['n']),
-            'e' => Base64Url::encode($theWrongKey['rsa']['e'])
+            'e' => Base64Url::encode($theWrongKey['rsa']['e']),
         ];
         $token = $this->sign($this->dpop);
 
         $dpop = new DPop();
         try {
-        	$dpop->validateDpop($token['token'], $this->serverRequest);
-	    } catch(RequiredConstraintsViolated $e) {
+            $dpop->validateDpop($token['token'], $this->serverRequest);
+        } catch(RequiredConstraintsViolated $e) {
             // need to check the actual violation in the exception, so expectExceptionMessage is not sufficient
-	    	$this->assertSame($e->violations()[0]->getMessage(),'Token signature mismatch');
-    	}
+            $this->assertSame($e->violations()[0]->getMessage(),'Token signature mismatch');
+        }
     }
 
     /**
